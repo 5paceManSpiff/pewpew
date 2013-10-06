1dc0360761d204a3cf1a4ed83bd446b32ad82c7f
5paceManSpiff <alspore@gmail.com>
2013-10-05 22:39:13 -0400

    added basic structure

---
+++ src/GameScene.hx
@@ -0,0 +1,22 @@
+import com.haxepunk.Scene;
+import com.haxepunk.HXP;
+
+import game.*;
+
+class GameScene extends Scene {
+
+  public function new() {
+    super(); 
+  }
+
+  public override function begin() {
+  }
+
+  public override function update() {
+    super.update();
+  }
+
+  public function reset() {
+
+  }
+}
---
+++ src/MenuScene.hx
@@ -0,0 +1,18 @@
+import com.haxepunk.Scene;
+import com.haxepunk.HXP;
+
+import game.*;
+
+class MenuScene extends Scene {
+
+  public function new() {
+    super(); 
+  }
+
+  public override function begin() {
+  }
+
+  public override function update() {
+    super.update();
+  }
+}
