4a61343b5b5164ecd2d4c297071bf8193e2ed2b3
5paceManSpiff <alspore@gmail.com>
2013-10-07 07:28:58 -0400

    added basic movement system
    implemented linear movement
    
    need to add acceleration and speed as well as ship rotation

--- src/game/Player.hx
+++ src/game/Player.hx
@@ -1,10 +1,10 @@
 package game;
 
-import Std;
 import com.haxepunk.Entity;
 import com.haxepunk.HXP;
 import com.haxepunk.graphics.Image;
 import com.haxepunk.utils.Input;
+import com.haxepunk.utils.Touch;
 
 class Player extends Entity {
 
@@ -18,6 +18,20 @@ class Player extends Entity {
   }
 
   public override function update() {
+    var touch0:Touch = Input.touches[0];
+    var touch1:Touch = Input.touches[1];
+
+    if(touch0 != null && touch1 != null) {
+      var xdist:Float = Math.abs(touch0.x - touch1.x);
+      var ydist:Float = Math.abs(touch0.y - touch1.y);
+
+      x = (touch0.x < touch1.x) ? touch0.x + xdist/2 : touch1.x + xdist/2;
+      y = (touch0.y < touch1.y) ? touch0.y + ydist/2 : touch1.y + ydist/2;
+
+      x -= sprite.width/2;
+      y -= sprite.height/2;
+    }
+
     super.update();
   }
 }
